2a3371d8c6f73e11a59d74f2986e7843ea832354
Martin Richter <richter91@gmail.com>
2015-01-27 18:34:59 +0100

    First task done

--- IPStat/src/ThreadRunner.java
+++ IPStat/src/ThreadRunner.java
@@ -1,10 +1,38 @@
-
 public class ThreadRunner {
-
+	
+	/**
+	 * Helper method that pauses the thread for 5 seconds
+	 */
+	public static void pause5(){
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException ie) {
+			System.out.println("Sleep error");
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			T1 thread1 = new T1();
-			T2 thread2 = new T2();
+		
+		T1 t1 = new T1();
+		pause5();
+		
+		T2 t2 = new T2();
+		pause5();
+		
+		t2.pause();
+		System.out.println("Thread 2 paused");
+		pause5();
+		
+		t2.unPause();
+		System.out.println("Thread 2 resumed");
+		pause5();
+		
+		t1.kill();
+		System.out.println("Killed thread 1");
+		pause5();
+		
+		t2.kill();
+		System.out.println("Killed thread 2");
 	}
 
 }
