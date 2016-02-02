255f80c0066fd4a9c669d73984bfd26748cbf9be
Martin Richter <richter91@gmail.com>
2016-02-02 22:18:31 +0100

    Added a couple of classes, started sketching layout

---
+++ IPStat/socketServer/ClientHandler.java
@@ -0,0 +1,21 @@
+
+public class ClientHandler implements Runnable{
+
+	//if message from client: call method to broadcast.
+	
+	
+	
+	public void run() {
+//		  String msg;
+//		  try {
+//		    while ((msg = reader.readLine()) != null) {
+//		      //... gör något med msg ...
+//		    }
+//		    writer.close();
+//		    reader.close();
+//		    socket.close();
+//		  } catch (IOException e) {
+//		  }
+//		  server.killThread(this);
+		}
+}
---
+++ IPStat/socketServer/ServerGUI.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.PrintWriter;
+
+import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+
+public class ServerGUI extends JFrame {
+	private JPanel panel;
+	private JTextArea textArea;
+	private JScrollPane scroll;
+
+	/**
+	 * Creates GUI
+	 */
+	public ServerGUI() {
+		createGUI();
+	}
+
+
+	/** Method for displaying text in textArea */
+	public void displayInput() {
+		//TODO skriva meddelande från klienter + från/till kopplingar
+	}
+
+	/** Method for setting window title */
+	public void updateWindowTitle() {
+		//TODO uppdatera antalet kopplade
+	}
+
+	private void createGUI() {
+		setSize(300, 300);
+
+		//BorderLayout so the fields adapt to window size
+		panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		JScrollPane scroll = new JScrollPane(textArea);
+		
+		//To make the textArea go to the bottom
+		DefaultCaret caret = (DefaultCaret)textArea.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+
+		panel.add(scroll, BorderLayout.CENTER);
+
+		this.add(panel);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
