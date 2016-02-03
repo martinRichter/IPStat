8c4ed4ae289f1123e1d5f9ef334e3275b5d1b206
Martin Richter <richter91@gmail.com>
2016-02-03 18:45:47 +0100

    Added new class. Struggling with how to handle multiple sockets.

--- IPStat/socketServer/ClientHandler.java
+++ IPStat/socketServer/ClientHandler.java
@@ -1,21 +1,60 @@
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
+	private int port;
 
-	//if message from client: call method to broadcast.
-	
-	
-	
-	public void run() {
-//		  String msg;
-//		  try {
-//		    while ((msg = reader.readLine()) != null) {
-//		      //... gör något med msg ...
-//		    }
-//		    writer.close();
-//		    reader.close();
-//		    socket.close();
-//		  } catch (IOException e) {
-//		  }
-//		  server.killThread(this);
+	public ClientHandler() {
+	}
+
+	private void example() {
+		ServerSocket server = null;
+
+		try {
+			server = new ServerSocket(port);
+			while (true) {
+				Socket connection = null;
+				try {
+					connection = server.accept();
+					Writer out = new OutputStreamWriter(
+							connection.getOutputStream());
+				} catch (IOException e) {
+				} finally {
+					try {
+						if (connection != null)
+							connection.close();
+					} catch (IOException e2) {
+					}
+				}
+			}
+		} catch (IOException e3) {
+			e3.printStackTrace();
+		} finally {
+			try {
+				if (server != null)
+					server.close();
+			} catch (IOException e4) {
+			}
 		}
+	}
+
+	// if message from client: call method to broadcast.
+
+	public void run() {
+		// String msg;
+		// try {
+		// while ((msg = reader.readLine()) != null) {
+		// //... gör något med msg ...
+		// }
+		// writer.close();
+		// reader.close();
+		// socket.close();
+		// } catch (IOException e) {
+		// }
+		// server.killThread(this);
+	}
 }
